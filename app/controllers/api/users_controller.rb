50752bc5157366fc390cef5227d2f93ef84dd6c9
Lizzy <lizzylibretti@gmail.com>
2019-01-22 13:26:29 -0500

    Create backend of auth

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,32 @@
+class Api::UsersController < ApplicationController 
+
+    def create
+        @user = User.new(user_params)
+        if @user.save!
+            login(@user)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+        render :show
+    end
+
+    def update
+        @user = User.find_by(id: params[:id])
+        if @user.update(user_params)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:email, :first_name, :password, :image_url)
+    end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?
+
+    def current_user 
+        return nil unless session[:session_token]
+        @current_user ||= User.find_by(session_token: session[:session_tokn])
+    end
+
+    def login(user)
+        session[:session_token] = user.reset_session_token
+        @current_user = user
+    end
+
+    def logout
+        
+        current_user.reset_session_token!
+        @current_user = nil
+        session[:session_token] = nil 
+    end
+
+    def logged_in? 
+        !!current_user
+    end
+
+    def require_logged_in
+        unless current_user
+            render json:{base: ['invalid credential']}, status 401
+        end
+    end
+    
 end
