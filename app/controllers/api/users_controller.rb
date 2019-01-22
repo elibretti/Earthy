d41a168478cb435fcb34a56f1a7aa9e8378c71ac
Lizzy <lizzylibretti@gmail.com>
2019-01-22 13:57:25 -0500

    Remove show and update from user backend, will implement later if time

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,20 +10,6 @@ class Api::UsersController < ApplicationController
         end
     end
 
-    def show
-        @user = User.find_by(id: params[:id])
-        render :show
-    end
-
-    def update
-        @user = User.find_by(id: params[:id])
-        if @user.update(user_params)
-            render :show
-        else
-            render json: @user.errors.full_messages, status: 422
-        end
-    end
-
     private
     
     def user_params
---
+++ frontend/util/session_api_util.js
@@ -0,0 +1,30 @@
+export const login = (user) => {
+    return $.ajax({
+        method: 'POST',
+        url: 'api/session',
+        data: {user}
+    })
+}
+
+export const signup = (user) => {
+    return $.ajax({
+        method: 'POST',
+        url: 'api/users',
+        data: {user}
+    })
+}
+
+export const logout = () => {
+    return $.ajax({
+        method: 'DELETE',
+        url: 'api/session'
+    })
+}
+
+export const updateUser = (user) => {
+    return $.ajax({
+        method: 'PATCH',
+        url: `api/users/${user.id}`
+        data{user}
+    })
+}
